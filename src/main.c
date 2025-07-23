165aa95e7f5b8a6b1d350d53af046ba75efdbd4b
Matti Henriksen <112562805+henrim3@users.noreply.github.com>
2025-07-23 16:46:52 -0400

    Basic input

--- src/main.c
+++ src/main.c
@@ -6,7 +6,10 @@
 
 #include "logging.h"
 
+#define INPUT_BUF_SIZE 256
+
 int saved_stdout = -1;
+char* input_buf = NULL;
 
 void disable_stdout() {
   LOG_MESSAGE( "TODO: implement disable_stdout" );
@@ -58,20 +61,114 @@ void check_python_installed() {
 
   if ( ( p_pipe = _popen( "python --version", "rt" ) ) == NULL ) {
     LOG_PERROR( "Error while running python --version" );
-    exit( 1 );
+    exit( EXIT_FAILURE );
   }
 
   if ( _pclose( p_pipe ) ) {
     LOG_ERROR( "Python not installed" );
-    exit( 1 );
+    exit( EXIT_FAILURE );
+  }
+}
+
+void init_input_buf() {
+  input_buf = malloc( INPUT_BUF_SIZE * sizeof( char ) );
+
+  if ( input_buf == NULL ) {
+    LOG_PERROR( "Error from malloc when initializing input buffer" );
+    exit( EXIT_FAILURE );
+  }
+}
+
+void print_prompt() {
+  printf( "yt-download-tool> " );
+}
+
+void get_input() {
+  if ( fgets( input_buf, INPUT_BUF_SIZE, stdin ) == NULL &&
+       ferror( stdin ) != 0 ) {
+    LOG_PERROR( "Error from fgets while getting input" );
+    exit( EXIT_FAILURE );
+  }
+}
+
+void strip_right( char* str ) {
+  int count = 0;
+  for ( int i = strlen( str ) - 1; i >= 0; i-- ) {
+    if ( str[i] != ' ' && str[i] != '\n' && str[i] != '\t' ) {
+      break;
+    }
+    count++;
+  }
+
+  str[strlen( str ) - count] = '\0';
+}
+
+// returns null terminated array
+char** get_tokens( char* str ) {
+  int arr_size = 8;
+  int arr_len = 0;
+  char** tokens = calloc( arr_size, sizeof( char* ) );
+
+  char* tok = strtok( input_buf, " " );
+  while ( tok != NULL ) {
+    // grow if necessary
+    if ( arr_len + 1 >= arr_size ) {
+      arr_size *= 2;
+      tokens = realloc( tokens, arr_size * sizeof( char* ) );
+    }
+
+    // copy to tokens
+    tokens[arr_len] = malloc( ( strlen( tok ) + 1 ) * sizeof( char ) );
+    strcpy( tokens[arr_len], tok );
+    arr_len++;
+
+    tok = strtok( NULL, " " );
+  }
+
+  tokens[arr_len] = NULL;
+
+  return tokens;
+}
+
+void free_null_terminated_str_arr( char** arr ) {
+  for ( char** ptr = arr; *ptr != NULL; ptr++ ) {
+    free( *ptr );
+  }
+  free( arr );
+}
+
+void handle_input() {
+  strip_right( input_buf );
+  char** tokens = get_tokens( input_buf );
+  for ( char** ptr = tokens; *ptr != NULL; ptr++ ) {
+    if ( strcmp( *ptr, "exit" ) == 0 ) {
+      exit( EXIT_SUCCESS );
+    }
+  }
+
+  free_null_terminated_str_arr( tokens );
+}
+
+void main_shell_loop() {
+  while ( 1 ) {
+    print_prompt();
+    get_input();
+
+    handle_input();
   }
 }
 
 int main() {
   LOG_MESSAGE( "Started yt-download-tool!\n" );
 
+  init_input_buf();
+
   // make sure python is installed
   check_python_installed();
+
+  main_shell_loop();
+
+  free( input_buf );
 
   return EXIT_SUCCESS;
 }
